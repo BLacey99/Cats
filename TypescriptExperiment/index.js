addfcc8b482300d33769b44c28df536a15aafe8a
BLacey99 <Blake.M.Lacey@gmail.com>
2023-01-09 23:00:11 -0600

    Fixed fetch retrieving undefined for cat photos/gifs. Added timeout. Possible server latency for that specific API causing rate limitations?

--- TypescriptExperiment/index.js
+++ TypescriptExperiment/index.js
@@ -3,59 +3,86 @@ let i = 1;
 let userInput = 15;
 let userAnimal = "cat";
 const arrayImages=[];
-const elem = document.createElement("img");
-elem.src = 'https://cataas.com/cat'; //Prints multiple copies of same cat due to fact that does batch checks on data verification.
 
-async function getImages(userAnimal, userInput){
-  while(arrayImages.length < userInput){
-    const responseImg = await fetch(
-      `https://cataas.com/${userAnimal}`
-    );
-    const dataImg = await responseImg.url;
-      //populated factoid with text node containing each data items text info at index [entry]. 
-          arrayImages.push(dataImg);
-          console.log(dataImg);
-}
-}
+ //Prints multiple copies of same cat due to fact that does batch checks on data verification.
+
+
+
+
+
+ async function getImages(userInput){
+  while (arrayImages.length < userInput) {
+    try{
+      const response = await fetch(
+        `https://cataas.com/cat?json=true`
+      );
+      // if(!response.ok){
+      //   throw new Error("HTTP status " + response.status);
+      // }
+      const data = await response.json();
+      for(entry in data) {
+        //populated factoid with text node containing each data items text info at index [entry].
+          if (arrayImages.length < userInput) {
+            if(arrayImages.includes(`https://cataas.com${data.url}` || `https://cataas.com${data.url="undefined"}`)){
+              continue;
+              }
+            else{
+              arrayImages.push(`https://cataas.com${data.url}`);
+            
+            }
+            
+     
+          } 
+    }
+   
+    }catch(error){
+      console.log(error);
+      continue;
+    }
+  }
+};
+
 async function getFacts(userAnimal, userInput){
-
     while (arrayBoi.length < userInput) {
       const response = await fetch(
         `https://cat-fact.herokuapp.com/facts/random?animal_type=${userAnimal}&amount=${userInput}`
       );
       const data = await response.json();
-      
       for(entry in data) {
-      
         //populated factoid with text node containing each data items text info at index [entry].
         if (data[entry].status.verified === true) {
-          const factoid = document.createElement("li");
-     
-          
           if (arrayBoi.length < userInput) {
-          
             arrayBoi.push(data[entry].text);
-            
-        
-            i++;
-           
           } else {
             break;
           }
         } else {
           continue;
         }
-        
       }
-      
     }
-    for(i = 0; i < userInput; i++){
+
+
+
+    setTimeout(() =>{
+    for(j = 0; j < userInput; j++){
       const factoid = document.createElement("li");
-      factoid.appendChild(document.createTextNode(`#${i+1}: ${arrayBoi[i]}`));
-              factoid.appendChild(elem); 
-              document.querySelector(".Facts").appendChild(factoid);
+      const elem = document.createElement("img");
+      elem.src = arrayImages[j];
+      factoid.appendChild(document.createTextNode(`#${j+1}: ${arrayBoi[j]}`));
+      
+      //setTimeout(() =>{
+        document.querySelector(".Facts").appendChild(factoid);
+        
+        document.querySelector(".Facts").appendChild(elem);
+        
+     // }, "2000")
+            
     }
+  }, "2000")};
 
-};
 
+
+
+getImages(userInput);
 getFacts(userAnimal, userInput);
