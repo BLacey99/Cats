206198b234251df845dc4ba0a4dda449ee30e16a
BLacey99 <Blake.M.Lacey@gmail.com>
2023-01-10 21:01:57 -0600

    Major refactor Code was too tightly coupled for object generation
    Created data builder which takes getImages and getFacts functions as callbacks, along with the two targeted arrays which these functions are storing data to.
    
    Created factory function inside of data builder. Builds cat object with properties of image and fact.
    Factory function catFactory pushes each cat object produced into a temporary array.
    
    Data builder function contains printData function which takes a callback factory function as its parameter
        printData awaits both callback functions from its parent function buildData to finish execution, then based on the length of one array, repeatedly feeds both arrays as the parameters for the factory.
        printData creates a new list item for each fact, and a new image for each image, and then appends them to a parent container called Facts.

--- TypescriptExperiment/index.js
+++ TypescriptExperiment/index.js
@@ -1,88 +1,102 @@
-const arrayBoi = [];
-let i = 1;
-let userInput = 15;
+let userInput = 10;
 let userAnimal = "cat";
-const arrayImages=[];
+const arrayImages = [];
+const arrayFacts = [];
+const jsonArray = [];
 
- //Prints multiple copies of same cat due to fact that does batch checks on data verification.
+async function getImages(userInput, arrImgs) {
+  while (arrImgs.length < userInput) {
+    try {
+      const response = await fetch(`https://cataas.com/cat?json=true`);
 
-
-
-
-
- async function getImages(userInput){
-  while (arrayImages.length < userInput) {
-    try{
-      const response = await fetch(
-        `https://cataas.com/cat?json=true`
-      );
-      // if(!response.ok){
-      //   throw new Error("HTTP status " + response.status);
-      // }
       const data = await response.json();
-      for(entry in data) {
+      for (entry in data) {
         //populated factoid with text node containing each data items text info at index [entry].
-          if (arrayImages.length < userInput) {
-            if(arrayImages.includes(`https://cataas.com${data.url}` || `https://cataas.com${data.url="undefined"}`)){
-              continue;
-              }
-            else{
-              arrayImages.push(`https://cataas.com${data.url}`);
-            
-            }
-            
-     
-          } 
-    }
-   
-    }catch(error){
+        if (arrImgs.length < userInput) {
+          if (
+            arrImgs.includes(
+              `https://cataas.com${data.url}` ||
+                `https://cataas.com${(data.url = "undefined")}`
+            )
+          ) {
+            continue;
+          } else {
+            arrImgs.push(`https://cataas.com${data.url}`);
+          }
+        } else {
+          break;
+        }
+      }
+    } catch (error) {
       console.log(error);
       continue;
     }
   }
-};
+  // console.log(arrImgs);
+}
 
-async function getFacts(userAnimal, userInput){
-    while (arrayBoi.length < userInput) {
-      const response = await fetch(
-        `https://cat-fact.herokuapp.com/facts/random?animal_type=${userAnimal}&amount=${userInput}`
-      );
-      const data = await response.json();
-      for(entry in data) {
-        //populated factoid with text node containing each data items text info at index [entry].
-        if (data[entry].status.verified === true) {
-          if (arrayBoi.length < userInput) {
-            arrayBoi.push(data[entry].text);
-          } else {
-            break;
-          }
+async function getFacts(userAnimal, userInput, arrFacts) {
+  while (arrFacts.length < userInput) {
+    const response = await fetch(
+      `https://cat-fact.herokuapp.com/facts/random?animal_type=${userAnimal}&amount=${userInput}`
+    );
+    const data = await response.json();
+    for (entry in data) {
+      //populated factoid with text node containing each data items text info at index [entry].
+      if (data[entry].status.verified === true) {
+        if (arrFacts.length < userInput) {
+          arrFacts.push(data[entry].text);
         } else {
-          continue;
+          break;
         }
+      } else {
+        continue;
       }
     }
+  }
+}
 
+//write function to store data into json objects
+const buildData = (ImageGetter, FactGetter, imgArr, factArr) => {
+  const catFactory = (image, fact) => {
+    const getImage = () => image;
+    const getFact = () => fact;
 
+    const cat = {
+      image: getImage(),
+      fact: getFact(),
+    };
 
-    setTimeout(() =>{
-    for(j = 0; j < userInput; j++){
+    const printCat = () => console.log(cat);
+    jsonArray.push(cat);
+    console.log(jsonArray);
+    printCat();
+  };
+
+  async function printData(factory) {
+    await ImageGetter, await FactGetter;
+
+    for (i = 0; i < imgArr.length; i++) {
+      factory(imgArr[i], factArr[i]);
+    
       const factoid = document.createElement("li");
       const elem = document.createElement("img");
-      elem.src = arrayImages[j];
-      factoid.appendChild(document.createTextNode(`#${j+1}: ${arrayBoi[j]}`));
-      
-      //setTimeout(() =>{
-        document.querySelector(".Facts").appendChild(factoid);
-        
-        document.querySelector(".Facts").appendChild(elem);
-        
-     // }, "2000")
-            
+      elem.src = imgArr[i];
+      factoid.appendChild(document.createTextNode(`#${i + 1}: ${factArr[i]}`));
+      document.querySelector(".Facts").appendChild(factoid);
+      document.querySelector(".Facts").appendChild(elem);
+      // console.log(imgArr[i]);
+      // console.log(factArr[i]);
     }
-  }, "2000")};
+  }
+  printData(catFactory);
+};
 
+//write function for creating list of items
 
-
-
-getImages(userInput);
-getFacts(userAnimal, userInput);
+buildData(
+  getImages(userInput, arrayImages),
+  getFacts(userAnimal, userInput, arrayFacts),
+  arrayImages,
+  arrayFacts
+);
